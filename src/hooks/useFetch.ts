052bb59d6e1d236e6b58431071a3166b08068fa6
Eric Lich <eric.lichtensztein@aleia.io>
2022-12-15 17:19:47 -0300

    initial commit with boilerplate

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { customFetch } from "../services/fetch";
+
+interface useFecthReturn<T> {
+  data: T[];
+  loading: boolean;
+  error: any;
+}
+
+export const useFetch = <T>(fetchUrl: string): useFecthReturn<T> => {
+  const [fetchReturn, setFetchReturn] = useState<useFecthReturn<T>>({
+    data: [],
+    error: undefined,
+    loading: false
+  })
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    setFetchReturn({ ...fetchReturn, loading: true });
+    customFetch(fetchUrl).then(res => {
+      setFetchReturn({ ...fetchReturn, data: [...fetchReturn.data, res], loading: false });
+    }).catch(err => setFetchReturn({ ...fetchReturn, error: err }));
+
+    return () => {
+      abortController.abort();
+    }
+  }, []);
+
+  return fetchReturn;
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,44 @@
+export const groupsTuple = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] as const;
+export const possibleGroupNumbers = [1, 2, 3, 4] as const;
+
+export type LetraGrupo = typeof groupsTuple[number];
+export type NumEquipoGrupo = typeof possibleGroupNumbers[number];
+
+export interface Grupo {
+  groupLetter: LetraGrupo,
+  groupTeamNumber: NumEquipoGrupo,
+}
+
+export type Team = {
+  Id: number;
+  nombre: string;
+  nombreCorto: string;
+  remera: string;
+  rankingFifa: string;
+  apuestas_2_puntaje: number;
+  prom_puntaje_apuestas: number;
+  ranking_ln: number;
+  urlTagEquipo: string;
+  repechaje: boolean;
+  btnAnexo: boolean;
+  grupo: Grupo; // -> its going to be some letter + num (1 to 4) -> A1 for example	
+}
+
+export type Match = {
+  fecha: string;
+  numero_partido: number;
+  hora: string;
+  grupo: LetraGrupo; // -> its going to be some letter + num (1 to 4) -> A1 for example	
+  instancia: string;
+  estadio: string;
+  tv: string;
+  sede: string;
+  ciudad: string;
+  idpartido: string;
+  equipoA: Grupo;
+  equipoB: Grupo;
+}
+
+export type Fixture = Match[];
+
+export type ApiResponseType<T> = T[];
